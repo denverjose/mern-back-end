17e2cc512c41118862ba486f95b4ff66841250b5
Denver Jose <denver.jose@stratpoint.com>
2023-01-20 02:54:25 +0800

    fixed cors

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const MONGO_DB = "mongodb+srv://denver:qweqwe@cluster0.yyuf74g.mongodb.net/final-project"
+const MONGO_DB =
+  "mongodb+srv://denver:qweqwe@cluster0.yyuf74g.mongodb.net/final-project";
 
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 
-const feedRoutes = require('./routes/feed');
-const userRoutes = require('./routes/user');
-const authRoutes = require('./routes/auth');
+const feedRoutes = require("./routes/feed");
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
 
 const app = express();
+app.use(cors());
 
- 
 const fileStorage = multer.diskStorage({
-  
-    destination: function(req, file, cb) {
-        cb(null, 'images/');
-    },
-    filename: function(req, file, cb) {
-        cb(null, `${uuidv4()}-${file.originalname}`)
-    }
+  destination: function (req, file, cb) {
+    cb(null, "images/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${uuidv4()}-${file.originalname}`);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -39,27 +39,24 @@ const fileFilter = (req, file, cb) => {
 };
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 // app.use("/images", express.static("images"))
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/user', userRoutes);
-app.use('/auth', authRoutes);
-
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-
+app.use("/feed", feedRoutes);
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -70,10 +67,8 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(
-    MONGO_DB
-  )
-  .then(result => {
+  .connect(MONGO_DB)
+  .then((result) => {
     app.listen(8080);
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
